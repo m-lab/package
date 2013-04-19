8c7a707d25c0163bc21aae813cc5a2b25750b1b7
stephen soltesz <stephen.soltesz@gmail.com>
2013-04-19 14:35:33 +0000

    remove 'submodule update' in favor of 'clone --recursive'

--- mlab/buildall.sh
+++ mlab/buildall.sh
@@ -14,7 +14,7 @@ while read slicetag repo slicename ; do
     test -d $slicename && rm -rf $slicename
 
     # NOTE: checkout repo
-    git clone $repo $slicename
+    git clone --recursive $repo $slicename
 
     # NOTE: set slice-specific source & build dirs
     export SOURCE_DIR=$PWD/$slicename
@@ -24,13 +24,8 @@ while read slicetag repo slicename ; do
         # NOTE: checkout the specific slicetag
         git checkout --quiet $slicetag
 
-        # NOTE: pull in submodules if present.
-        if test -f .gitmodules ; then
-            git submodule update --init --recursive 
-        fi
-
         # NOTE: perform build & pass 'export'd vars through environment
-        ./package/buildslice.sh $slicename
+        ./package/slicebuild.sh $slicename
     popd
 done
 
